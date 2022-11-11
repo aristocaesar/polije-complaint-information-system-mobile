bc658e38e60be5d2e9987a1a057e40f99df5a670
aristocaesar <hi@aristoc.space>
2022-11-11 12:40:19 +0700

    build component drawer, splashscreen and landing page

---
+++ lib/component/drawer.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class DrawerComponent extends StatefulWidget {
+  const DrawerComponent({super.key});
+
+  @override
+  State<DrawerComponent> createState() => _DrawerComponentState();
+}
+
+class _DrawerComponentState extends State<DrawerComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(15, 76, 117, 100),
+              ),
+              child: UserAccountsDrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(15, 76, 117, 100),
+                ),
+                currentAccountPicture: ClipOval(
+                  child: FlutterLogo(
+                    size: 300,
+                    textColor: Colors.blue,
+                    style: FlutterLogoStyle.stacked,
+                  ),
+                ),
+                accountName: Text(
+                  'Ferdy Sambo',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontFamily: "Poppins"),
+                ),
+                accountEmail: Text(
+                  'ferdysambo@kapolri.gov.id',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white,
+                      fontFamily: "Poppins"),
+                ),
+              )),
+          ListTile(
+            title: const Text(
+              'Layanan',
+              style: TextStyle(fontFamily: "Poppins"),
+            ),
+            leading: const Icon(Icons.layers),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text(
+              'Laporan',
+              style: TextStyle(fontFamily: "Poppins"),
+            ),
+            leading: const Icon(Icons.archive),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('Pengaturan',
+                style: TextStyle(fontFamily: "Poppins")),
+            leading: const Icon(Icons.settings),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title:
+                const Text('Tentang', style: TextStyle(fontFamily: "Poppins")),
+            leading: const Icon(Icons.info),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title:
+                const Text('Keluar', style: TextStyle(fontFamily: "Poppins")),
+            leading: const Icon(Icons.exit_to_app),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:polije_complaint_information_system_mobile/component/drawer.dart';
 
 class Landing extends StatefulWidget {
   const Landing({super.key});
@@ -8,10 +9,54 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Landing Page"),
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text("POLIJE",
+            style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 1,
+                fontFamily: "Poppins")),
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromRGBO(15, 76, 117, 100),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.white),
+                  onPressed: () => _scaffoldKey.currentState?.openDrawer())),
+        ],
+      ),
+      drawer: const DrawerComponent(),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("/images/background-app.png"),
+                fit: BoxFit.fill)),
+        child: ListView(
+          children: const <Widget>[
+            SizedBox(height: 80),
+            Center(
+              child: Text(
+                "Layanan",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 80),
+          ],
+        ),
+      ),
     );
   }
 }
